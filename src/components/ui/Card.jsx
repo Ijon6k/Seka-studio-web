4fbda4415a751d86aceddcb4cb8f27740a1597f2
Ijon6k <erikghafari@gmail.com>
2024-06-28 20:48:45 +0700

    forgot to change prop

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -2,11 +2,7 @@ const Card = ({ title, image }) => {
   return (
     <div className="w-fit h-fit ">
       <div className="w-64 h-72">
-        <img
-          src="../../../public/images/product1.png"
-          alt=""
-          className="w-full h-full"
-        />
+        <img src={image} alt={title} className="w-full h-full" />
       </div>
       <h1 className="font-bold text-center text-black pt-5">{title}</h1>
     </div>
