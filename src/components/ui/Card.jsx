289a1b755cf32be723684641a6a9c5643cfb68b3
Ijon6k <erikghafari@gmail.com>
2024-06-28 20:37:55 +0700

    change the image prop to product image url directly

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -2,7 +2,11 @@ const Card = ({ title, image }) => {
   return (
     <div className="w-fit h-fit ">
       <div className="w-64 h-72">
-        <img src={image} alt="" className="w-full h-full" />
+        <img
+          src="/public/images/product1.png"
+          alt=""
+          className="w-full h-full"
+        />
       </div>
       <h1 className="font-bold text-center text-black pt-5">{title}</h1>
     </div>
