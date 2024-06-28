6324cc39fd0048b0a7b12cb3f4fb070996881995
Ijon6k <erikghafari@gmail.com>
2024-06-28 17:45:18 +0700

    feat: added card section tentang kami section

--- src/components/layout/PageWrapper.jsx
+++ src/components/layout/PageWrapper.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 const PageWrapper = ({ children, customClass }) => {
   return (
     <div
-      className={`${customClass} pt-16  max-w-screen-2xl mx-auto w-screen h-auto min-h-screen`}
+      className={`${customClass} pt-20  max-w-screen-2xl mx-auto w-screen h-auto min-h-screen`}
     >
       <Navbar></Navbar>
       {children}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,43 @@
+import HeroSection from "../components/layout/HeroSection";
 import PageWrapper from "../components/layout/PageWrapper";
+import Card from "../components/ui/Card";
+import data from "../ProductInterest.json";
 
 const Home = () => {
   return (
     <PageWrapper customClass={""}>
-      <div>Home</div>
+      <HeroSection></HeroSection>
+      <section className="grid grid-cols-3 place-items-center max-w-screen-xl w-screen  p-10 gap-20 ">
+        {data.map((course, index) => (
+          <Card key={index} image={course.image} title={course.title} />
+        ))}
+      </section>
+      {/* tentang kami */}
+      <section className="bg-black/50 h-96 w-full flex ">
+        <div className="w-fit m-auto px-20 flex justify-center flex-col">
+          {" "}
+          <h1 className="text-center text-white font-bold text-3xl pb-5">
+            T<span className="text-red-600">e</span>ntang Kami
+          </h1>
+          <p className="text-white font-semibold ">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil
+            quibusdam nulla quis, fugit libero porro ipsum reprehenderit
+            doloribus dolor veniam nostrum voluptas molestias vel, quos quae
+            odio. Quia et molestiae ex, placeat, fuga tempore obcaecati aliquid,
+            repellendus doloribus perspiciatis impedit ipsa necessitatibus odio
+            harum possimus exercitationem minima. Impedit eligendi esse minima
+            quasi, perferendis magni itaque sed tenetur adipisci, reiciendis
+            unde, sit iusto quibusdam accusamus earum odit veritatis error totam
+            aliquid ex vitae ipsum incidunt ratione? Sapiente tempore molestiae
+            tempora iusto fugiat commodi, rerum deleniti pariatur voluptates
+            minima nesciunt quas praesentium, quidem quis ullam quisquam sit
+            doloremque quasi, ea rem eligendi.
+          </p>
+          <button className="bg-black/50 shadow-md rounded-xl py-2 font-semibold mt-5 px-3 text-white mx-auto">
+            Read more
+          </button>
+        </div>
+      </section>
     </PageWrapper>
   );
 };
